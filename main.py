8d385d070341bef48b8fa1a0c45b22d7afc2a2e0
kshitij kumar singh <kshitij22@live.com>
2018-07-27 21:29:03 +0530

    rooms are connected by corridors now :D

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 W =60
 H =60
 MAX_ROOMS = 12
@@ -33,7 +35,7 @@ def check_collides(room, rooms):
 
 
 def room():
-	from random import randint
+
 	room = {}
 	room['x'] = randint(1,W-10);
 	room['y'] = randint(1,H-10);
@@ -59,6 +61,50 @@ def get_rooms():
 
 	return rooms
 
+
+def find_closest_room(R, rooms):
+	mid_point = { 'x': R['x']+(R['w']/2), 'y': R['y']+(R['h']/2) }
+	closest_distance = 1000000
+	closest_room = {}
+	for i in rooms:
+		if i == R:continue
+		i_mid = { 'x': R['x']+(R['w']/2), 'y': R['y']+(R['h']/2) } 
+		distance = abs(mid_point['x'] - i_mid['x']) + abs(mid_point['y'] - i_mid['y'])
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_room = i
+	return closest_room
+			 
+		
+ 
+def build_coridors(G, rooms):
+	for i in rooms:
+		roomA = i
+		roomB = find_closest_room(roomA, rooms) 	
+		
+		# Find random points in the current room & closest room
+
+		pointA = { 'x': randint(roomA['x'] , roomA['x'] + roomA['w']) , 'y': randint(roomA['y'] , roomA['y']+ roomA['h'])}
+		pointB = { 'x': randint(roomB['x'] , roomB['x']+ roomB['w']) , 'y': randint(roomB['y'] ,  roomB['y'] + roomB['h'])}
+
+
+		while (pointB['x'] != pointA['x']) or (pointB['y'] != pointA['y']) :         
+			if pointB['x'] != pointA['x']:             
+			    if pointB['x'] > pointA['x']:
+				pointB['x'] -= 1
+			    else:
+				pointB['x'] += 1
+			elif pointB['y'] != pointA['y']:
+			    if pointB['y'] > pointA['y']:
+				pointB['y'] -= 1
+			    else:
+	 			pointB['y'] +=1 
+		
+			print pointB['x'],pointB['y']
+			G[pointB['x']][pointB['y']] = 'x'
+		
+	return G
+		
 def render(G,rooms):
 	for i in rooms:
 		x = i['x']
@@ -77,4 +123,7 @@ G = grid()
 
 rooms = get_rooms()
 
+print_map(render(G,rooms))
+
+G  = build_coridors(G,rooms)
 print_map(render(G,rooms))
